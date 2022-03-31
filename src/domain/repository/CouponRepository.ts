f5f080ab8c80b002661962f7a6c824c863aeceb9
Leonardo Treviso <leonardosistemas@outlook.com>
2022-03-30 22:21:08 -0300

    refactor: ajustando a classe de cupom repository

--- src/domain/repository/CouponRepository.ts
+++ src/domain/repository/CouponRepository.ts
@@ -1,5 +1,5 @@
 import Coupon from "../entity/Coupon";
 
 export default interface CuponRepository {
-    getByCode(code:string) : Coupon | undefined;
+    getByCode(code: string): Promise<Coupon | undefined>;
 }
